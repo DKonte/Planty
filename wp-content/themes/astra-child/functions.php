e55969226ea993a61a7a640cda6ef4c700455fd8
DKonte <164095967+DKonte@users.noreply.github.com>
2024-05-14 12:07:58 +0200

    gestion du responsive

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -22,4 +22,34 @@ if ( !function_exists( 'child_theme_configurator_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
+function custom_menu_items($items, $args) {
+    if ($args->theme_location === 'primary') {
+        if (is_user_logged_in()) {
+            // Construit manuellement le lien 'Admin'
+            $admin_link = '<li id="menu-item-admin" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-admin">';
+            $admin_link .= '<a href="http://localhost/Planty/wp-admin/index.php" class="admin-button">Admin</a>';
+            $admin_link .= '</li>';
+
+            // Trouve l'ID du deuxième élément ('Commander') dans le menu
+            $order_id = wp_filter_object_list(wp_get_nav_menu_items('Principal'), array('title' => 'Commander'));
+            $order_id = reset($order_id)->ID;
+
+            // Trouve la position du deuxième élément dans le menu
+            $position = strpos($items, '<li id="menu-item-' . $order_id . '"');
+
+            // Insère le lien 'Admin' avant le deuxième élément
+            if ($position !== false) {
+                $items = substr_replace($items, $admin_link, $position, 0);
+            }
+        }
+
+        return $items;
+    }
+
+    if ($args->theme_location === 'footer_menu') {
+        return $items;
+    }
+}
+
+add_filter('wp_nav_menu_items', 'custom_menu_items', 10, 2);
 // END ENQUEUE PARENT ACTION
